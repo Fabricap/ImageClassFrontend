3ad956ef9eaab6550788020112dd17badc965c55
Fabrizio Capeccia <fabrizio.capeccia@outlook.it>
2022-09-08 12:42:11 +0200

    added opencv requirement headless to make it work

--- fabri_frontend.py
+++ fabri_frontend.py
@@ -19,6 +19,28 @@ from PIL import Image
 
 app = hy.HydraApp(title='Img Detector',favicon="camera",hide_streamlit_markers=True,use_navbar=True, navbar_sticky=True)
 
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/jpg;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+#set_png_as_page_bg('')
+
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
